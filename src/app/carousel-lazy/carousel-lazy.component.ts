6f27e08d8416d6758ed9d6ba5ad3ea01525910f9
Andriy Orehov <andriyorehov@gmail.com>
2020-01-28 10:42:22 +0200

    implement lazy carousel images

--- src/app/carousel-lazy/carousel-lazy.component.ts
+++ src/app/carousel-lazy/carousel-lazy.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { Item } from '../models/item';
+import { DateMode } from '../models/types';
 import {animate, AnimationBuilder, AnimationFactory, AnimationPlayer, style} from '@angular/animations';
 
 @Component({
@@ -25,12 +26,24 @@ export class CarouselLazyComponent implements OnInit {
     ]);
   }
 
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   next() {
-    if ( this.currentSlide + 1 === this.items.length ) {
-      this.currentSlide = 0
-    } else {
-      this.currentSlide = (this.currentSlide + 1) % this.items.length;
-    }
+    if ( this.currentSlide + 1 === this.items.length ) { return; }
+    this.currentSlide = (this.currentSlide + 1) % this.items.length;
+
+    const modeKeys = Object.keys(DateMode);
+    const randomMode = modeKeys[Math.floor(Math.random() * modeKeys.length)];
+    const randomImageIndex = this.getRandomInt(1, 150);
+    this.items.push({
+      title: Date.now(),
+      mode: DateMode[randomMode],
+      src: `https://picsum.photos/id/${randomImageIndex}/900/500`
+    });
 
     const offset = this.currentSlide * this.itemWidth;
     const myAnimation: AnimationFactory = this.buildAnimation(offset);
@@ -39,11 +52,9 @@ export class CarouselLazyComponent implements OnInit {
   }
 
   prev() {
-    if ( this.currentSlide === 0 ) {
-      this.currentSlide = this.items.length - 1;
-    } else {
-      this.currentSlide = ((this.currentSlide - 1) + this.items.length) % this.items.length;
-    }
+    if (this.currentSlide === 0 ) { return; }
+    this.currentSlide = ((this.currentSlide - 1) + this.items.length) % this.items.length;
+    this.items.pop();
 
     const offset = this.currentSlide * this.itemWidth;
     const myAnimation: AnimationFactory = this.buildAnimation(offset);
